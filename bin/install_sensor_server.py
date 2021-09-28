4a0bb5ad78fa0820ae5029409ce375b0107c4111
dskrypa <dskrypa@gmail.com>
2021-09-28 14:41:17 -0400

    finished service install script

--- bin/install_sensor_server.py
+++ bin/install_sensor_server.py
@@ -8,7 +8,9 @@ sys.path.insert(0, PROJECT_ROOT.joinpath('bin').as_posix())
 import _venv  # This will activate the venv, if it exists and is not already active
 
 import logging
+import os
 from configparser import ConfigParser
+from subprocess import check_call
 
 sys.path.append(PROJECT_ROOT.joinpath('lib').as_posix())
 from ds_tools.argparsing import ArgParser
@@ -20,6 +22,7 @@ log = logging.getLogger(__name__)
 
 def parser():
     parser = ArgParser(description='Temperature Sensor Server Installer\n\nInstall temp_sensor.py via systemd')
+    parser.add_argument('--name', '-n', default='temp_sensor_server', help='The name of the systemd service to create')
     parser.include_common_args('verbosity')
     return parser
 
@@ -29,21 +32,43 @@ def main():
     args = parser().parse_args()
     init_logging(args.verbose)
 
+    systemd_dir = Path('/etc/systemd/system')
+    if not systemd_dir.exists():
+        raise RuntimeError(f'Could not find {systemd_dir.as_posix()} - {Path(__file__).name} only supports systemd')
 
-def create_unit_file():
+    systemd_dir_stat = systemd_dir.stat()
+    if systemd_dir_stat.st_uid != os.getuid() or systemd_dir_stat.st_gid != os.getgid():
+        raise RuntimeError(f'You must run `sudo {Path(__file__).name}` to proceed')
+
+    create_unit_file(args.name)
+    enable_and_start(args.name)
+
+
+def create_unit_file(name: str):
     config = ConfigParser()
     config.optionxform = str  # Preserve case
 
-    # for section in ('Unit', 'Service', 'Install'):
-    #     config.add_section(section)
-
-    server_path = PROJECT_ROOT.joinpath('bin', 'temp_sensor.py')
-
-
+    server_path = PROJECT_ROOT.joinpath('bin', 'temp_sensor.sh')
     config['Unit'] = {'Description': 'Temperature & humidity sensor server', 'After': 'multi-user.target'}
-    config['Service'] = {'Type': 'simple', 'ExecStart': None}
+    config['Service'] = {'Type': 'simple', 'ExecStart': f'/bin/bash {server_path.as_posix()}'}
     config['Install'] = {'WantedBy': 'multi-user.target'}
 
+    unit_file_path = Path(f'/etc/systemd/system/{name}.service')
+    if unit_file_path.exists():
+        raise RuntimeError(f'Unit file {unit_file_path.as_posix()} already exists')
+
+    log.info(f'Creating {unit_file_path.as_posix()}')
+    with unit_file_path.open('w', encoding='utf-8') as f:
+        config.write(f)
+    unit_file_path.chmod(0o644)
+
+
+def enable_and_start(name: str):
+    log.info(f'Starting service={name}')
+    check_call(['systemctl', 'start', name])
+    log.info(f'Enabling service={name} to run at boot')
+    check_call(['systemctl', 'enable', name])
+    log.info(f'\n\nRun `sudo reboot` to reboot and ensure installation was successful')
 
 
 if __name__ == '__main__':
