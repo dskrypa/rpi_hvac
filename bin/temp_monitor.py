b288364a4b82502e95ded3fca7eeeaf50a9a3b4f
dskrypa <dskrypa@gmail.com>
2021-09-28 14:13:33 -0400

    refactored monitor from giant loop to class

--- bin/temp_monitor.py
+++ bin/temp_monitor.py
@@ -9,10 +9,13 @@ import _venv  # This will activate the venv, if it exists and is not already act
 
 import logging
 import time
-from requests import Session
+from datetime import datetime, timedelta
+from requests import Session, RequestException
 
 sys.path.append(PROJECT_ROOT.joinpath('lib').as_posix())
 from rpi_hvac.__version__ import __author_email__, __version__
+from rpi_hvac.nest.client import NestWebClient
+from tz_aware_dt.tz_aware_dt import TZ_LOCAL
 from ds_tools.argparsing import ArgParser
 from ds_tools.core.main import wrap_main
 from ds_tools.logging import init_logging, ENTRY_FMT_DETAILED
@@ -36,49 +39,81 @@ def main():
     args = parser().parse_args(req_subparser_value=True)
     init_logging(args.verbose, names_add=['rpi_hvac'], entry_fmt=ENTRY_FMT_DETAILED)
 
-    from rpi_hvac.nest.client import NestWebClient
-    nest = NestWebClient(config_path=args.config, reauth=args.reauth)
+    monitor = TempMonitor(args.server, args.delay, args.config, args.reauth, celsius=args.celsius)
+    monitor.run()
 
-    delay = args.delay
-    url = f'http://{args.server}/read'
-    session = Session()
-    last_temp = 100
-    last_nest_check = 0
-    nest_check_freq = 180  # 3 minutes
-    # nest_status = None
-    mode, running, current, target = None, False, None, None
-    while True:
-        log.debug(f'GET -> {url}')
-        resp = session.get(url)
-        log.debug(f'Response: {resp} - {resp.text}')
-        if not resp.ok:
-            log.error(f'Error reading temperature from server: {resp} - {resp.text}')
+
+class TempMonitor:
+    def __init__(
+        self, server: str, delay: int, nest_config, nest_reauth, nest_check_freq: int = 180, celsius: bool = False
+    ):
+        self.url = f'http://{server}/read'
+        self.session = Session()
+        self.nest = NestWebClient(config_path=nest_config, reauth=nest_reauth)
+        self.nest_check_freq = timedelta(seconds=nest_check_freq)
+        self.last_temp = 100
+        self.last_nest_check = datetime.now(TZ_LOCAL) - timedelta(seconds=nest_check_freq + 1)
+        self.delay = delay
+        self.celsius = celsius
+        self.nest_mode = None
+        self.nest_running = False
+        self.nest_current = None
+        self.nest_target = None
+
+    def read_sensor(self):
+        log.debug(f'GET -> {self.url}')
+        try:
+            resp = self.session.get(self.url, timeout=10)
+        except RequestException as e:
+            raise ReadRequestError(f'Error reading temperature from server: {e}') from e
         else:
-            data = resp.json()
-            humidity, temp_c = data['humidity'], data['temperature']
-            temp_f = temp_c * 9/5 + 32
-            increasing = temp_c > last_temp
-            last_temp = temp_c
+            log.debug(f'Response: {resp} - {resp.text}')
+            if resp.ok:
+                return resp.json()
+            raise ReadRequestError(f'Error reading temperature from server: {resp} - {resp.text}')
 
-            next_nest_check = last_nest_check + nest_check_freq - time.monotonic()
-            if increasing and next_nest_check <= 0:
-                next_nest_check = 'updated'
-                last_nest_check = time.monotonic()
-                nest_status = nest.get_state(fahrenheit=not args.celsius)
-                running = nest_status['fan_current_speed'] == 'off'
-                mode = nest_status['current_schedule_mode'].upper()
-                current = nest_status['current_temperature']
-                target = nest_status['target_temperature']
+    def update_nest_status(self):
+        nest_status = self.nest.get_state(fahrenheit=not self.celsius)
+        self.last_nest_check = datetime.now(TZ_LOCAL)
+        self.nest_running = nest_status['fan_current_speed'] == 'off'
+        self.nest_mode = nest_status['current_schedule_mode'].upper()
+        self.nest_current = nest_status['current_temperature']
+        self.nest_target = nest_status['target_temperature']
 
-            message = f'{temp_f=:.2f} F / {temp_c=:.2f} C - {increasing=} | {humidity=:.2f}%'
-            if mode is not None:
-                message += f' | Nest {mode=} {running=} {current=:.2f} {target=:.2f} (next check in: {next_nest_check} s)'
+    def _process_status(self, data):
+        humidity, temp_c = data['humidity'], data['temperature']
+        temp_f = temp_c * 9 / 5 + 32
+        increasing = temp_c > self.last_temp
+        self.last_temp = temp_c
+
+        next_nest_check = self.last_nest_check + self.nest_check_freq - datetime.now(TZ_LOCAL)
+        if increasing and next_nest_check.total_seconds() <= 0:
+            self.update_nest_status()
+
+        message = f'{temp_f=:.2f} F / {temp_c=:.2f} C - {increasing=} | {humidity=:.2f}%'
+        if self.nest_mode is not None:
+            message += (
+                f' | Nest mode={self.nest_mode} running={self.nest_running} current={self.nest_current:.2f}'
+                f' target={self.nest_target:.2f} (last check: {self.last_nest_check.isoformat(" ")})'
+            )
+        else:
+            message += ' | Nest mode=? running=? current=? target=? (last check: -)'
+
+        log.info(message)
+
+    def run(self):
+        while True:
+            try:
+                data = self.read_sensor()
+            except ReadRequestError as e:
+                log.error(e)
             else:
-                message += ' | Nest mode=? running=? current=? target=? (next check in: ? s)'
+                self._process_status(data)
+            time.sleep(self.delay)
 
-            log.info(message)
 
-        time.sleep(delay)
+class ReadRequestError(Exception):
+    """Exception to be raised when a read request could not be completed"""
 
 
 if __name__ == '__main__':
