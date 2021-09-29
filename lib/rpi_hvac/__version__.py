37346b604e50e7bb36dedb8a70d069d6faa3e089
dskrypa <dskrypa@gmail.com>
2021-09-29 11:08:28 -0400

    in-lined some of the code from the adafruit_dht lib to work around lib oddities

--- lib/rpi_hvac/__version__.py
+++ lib/rpi_hvac/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'rpi_hvac'
 __description__ = 'HVAC control via Raspberry Pi'
 __url__ = 'https://github.com/dskrypa/rpi_hvac'
-__version__ = '2021.09.29-1'
+__version__ = '2021.09.29-2'
 __author__ = 'Doug Skrypa'
 __author_email__ = 'dskrypa@gmail.com'
 __copyright__ = 'Copyright 2021 Doug Skrypa'
--- lib/rpi_hvac/rpi.py
+++ lib/rpi_hvac/rpi.py
@@ -4,7 +4,9 @@ Utilities for working with the Raspberry Pi's SenseHat
 :author: Doug Skrypa
 """
 
+import array
 import logging
+import time
 
 from adafruit_dht import DHT22
 from board import D4  # noqa
@@ -20,27 +22,75 @@ except ImportError:
 
 __all__ = ['EnvSensor', 'Dht22Sensor']
 log = logging.getLogger(__name__)
+READ_DELAY = 2
 
 
 class Dht22Sensor:
     def __init__(self, max_retries: int = 4):
         self.sensor = DHT22(D4, False)
         self.max_retries = max_retries
+        self._last = 0
+
+    def measure(self) -> tuple[float, float]:
+        to_wait = READ_DELAY - (time.monotonic() - self._last)
+        if to_wait > 0:
+            log.debug(f'Waiting {to_wait:.3f} s before reading sensor')
+            time.sleep(to_wait)
+
+        pulses = self.sensor._get_pulses_bitbang()
+        self._last = time.monotonic()
+        if len(pulses) < 10:  # Probably a connection issue
+            raise SensorReadFailed('DHT sensor not found - check wiring')
+        elif len(pulses) < 80:  # We got *some* data just not 81 bits
+            raise SensorReadFailed(f'A full buffer was not returned - only received {len(pulses)} bits - try again')
+
+        buf = array.array('B')
+        for byte_start in range(0, 80, 16):
+            buf.append(self.sensor._pulses_to_binary(pulses, byte_start, byte_start + 16))
+
+        if sum(buf[0:4]) & 0xFF != buf[4]:
+            raise SensorReadFailed('Checksum did not validate - try again')
+
+        humidity = ((buf[0] << 8) | buf[1]) / 10
+        # temperature is 2 bytes; MSB is sign, bits 0-14 are magnitude)
+        temperature = (((buf[2] & 0x7F) << 8) | buf[3]) / 10
+        if buf[2] & 0x80:
+            temperature = -temperature
+
+        if not 0 < humidity < 100:
+            raise RuntimeError(f'Received implausible data ({temperature=}, {humidity=}) - try again')
+
+        return humidity, temperature
 
     def read(self) -> tuple[float, float]:
-        retries = self.max_retries
+        if (retries := self.max_retries) < 0:
+            retries = 1
+
+        while True:
+            try:
+                return self.measure()
+            except SensorReadFailed:
+                retries -= 1
+                if retries <= 0:
+                    raise
+
+    def read_old(self) -> tuple[float, float]:
+        if (retries := self.max_retries) < 0:
+            retries = 1
         sensor = self.sensor
-        last_exc = None
-        while retries > 0:
+        while True:
             try:
                 sensor.measure()
             except RuntimeError as e:
                 retries -= 1
-                last_exc = e
+                if retries <= 0:
+                    raise SensorReadFailed(f'Failed to read sensor - {e}')
             else:
-                if sensor._humidity is not None:
-                    return sensor._humidity, sensor._temperature
-        raise SensorReadFailed(f'Failed to read sensor - {last_exc}')
+                if sensor._humidity is None:
+                    retries -= 1
+                    if retries <= 0:
+                        raise SensorReadFailed(f'Failed to read sensor - invalid values')
+                return sensor._humidity, sensor._temperature
 
 
 class EnvSensor:
